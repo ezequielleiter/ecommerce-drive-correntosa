470c5e2fd11e7429c8b2333fec9fe14d9cd9d090
ezequielleiter <ezequielleiter@gmail.com>
2023-03-29 13:27:58 -0300

    fix db connection

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,6 +1,6 @@
 import { singleton } from 'tsyringe';
 import config from '../../constants/config';
-import { connection, connect, ConnectionStates } from 'mongoose';
+import { connection, connect, ConnectionStates, ConnectOptions } from 'mongoose';
 
 @singleton()
 class BaseService {
@@ -14,8 +14,11 @@ class BaseService {
 		return new Promise(async (resolve, reject) => {
 			try {
 				if (this.isDbConnected) return;
-				const db = await connect(`mongodb+srv://${config.mongo.MONGO_USERNAME}:${config.mongo.MONGO_PASSWORD}@${config.mongo.MONGO_HOST}/${config.mongo.MONGO_DATABASE}?tls=true&authSource=admin&replicaSet=db-app-correntosa`);
-
+				const db = await connect(`mongodb+srv://${config.mongo.MONGO_USERNAME}:${config.mongo.MONGO_PASSWORD}@${config.mongo.MONGO_HOST}/${config.mongo.MONGO_DATABASE}?retryWrites=true&w=majority`, {
+					useNewUrlParser: true,
+					useUnifiedTopology: true,
+					authSource: 'admin'
+				} as ConnectOptions);
 				this.isDbConnected = db.connections[0].readyState;
 				resolve(db.connections[0].readyState);
 			} catch (e) {
